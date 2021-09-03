64fac18059a40d0672a4fb632a9e80b222bb5853
WeilanTao <64599029+WeilanTao@users.noreply.github.com>
2021-09-03 09:05:56 -0400

    singleton v1

--- src/Singleton/Exercise.java
+++ src/Singleton/Exercise.java
@@ -6,7 +6,7 @@ import java.util.function.Supplier;
  *  Supplier<T>接口被称为是一个生产型接口，指定接口的泛型是什么类型，那么接口中get方法获取的就是什么类型的数据.
  *
  *  isSingleton is used to check whether a factory method func returns a singleton instance.
- *  
+ *
  *  In the method isSingleton, func is a method that returns an Object, which is an instance of a class.
  *
  */
